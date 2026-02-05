3a43172bae1fd7680c59621f829182d8e79fbe8d
Shan Diamond Louis <shan.diamondlouis@gmail.com>
2026-02-04 23:05:20 -0500

    Replaced launch-template-name with template-id

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,3 +12,11 @@ output "prod_subnet_id" {
   description = "ID of the production public subnet"
   value       = module.vpc.prod_public_id
 }
+
+output "staging_launch_template_id" {
+  value = module.asg_staging.launch_template_id
+}
+
+output "production_launch_template_id" {
+  value = module.asg_production.launch_template_id
+}
